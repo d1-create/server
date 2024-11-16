f05e23964eb3e07272e7cf61830cb5e9e5a98edb
Dirushan Uthayamoorthy <116972461+d1-create@users.noreply.github.com>
2024-11-16 11:45:21 +0000

    myserver

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -Xms4G -Xmx12G -jar fabric-server-mc.1.20.1-loader.0.16.9-launcher.1.0.1.jar
+java -XX:+UseShenandoahGC -Xms2G -Xmx10G -jar fabric-server-mc.1.20.4-loader.0.16.9-launcher.1.0.1.jar nogui
